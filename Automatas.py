c305bfaaf65717d65e18896cb3d8c744fe5b2f28
eidepozo <eidepozo@hotmail.com>
2016-04-02 16:55:38 -0300

    Avanze un poco en la forma de interactuar con el usuario para que vayan cachando, ahora creo que metere un poco de interfaz grafica, eso saludines

--- Automatas.py
+++ Automatas.py
@@ -1,13 +1,42 @@
+"""La tarea consiste en diseñar y construir un programa computacional capaz de
+simular cualquier AFD"""
+#La entrada al programa consiste en: estado inicial, estados finales
+#transiciones del AFD, palabra de entrada
+
+#La salida un mensaje indicando si pertenece o no al lenguaje
+
+#El programa debe poder analizar muchas palabras de entrada
+#Debe ser autoexplicativo
+
+
 class Estado:
-	def __init__(self, transicion, puntero, final):
-		self.final = final #True or False
-		self.vinculo = []
-		
-	def addPuntero(self, transicion, puntero):
-		self.vinculo.append([tansicion, puntero])
-		
-class Estado_Inicial(Estado):
-	def __init__(self, transicion, puntero, final):
-		self.inicial = True
-		self.final = final #True or False
-		self.vinculo = []
+	def __init__(self, nombre):
+		self.nombre = nombre
+
+
+
+print "-----------Teoria de Automatas--------------\nSimulador de Automatas Finitos Deterministas\nAutores: Elliot Ide, Gabriel Galilea, Javier Godoy"
+print "*********************************************"
+
+#variables
+listaestados = [] #lista de objetos tipo estado
+
+
+n = input("Ingrese numero de estados (Debe ser mayor a 0): ")
+while n<=0:
+        n = input("Ingrese numero de estados (Debe ser mayor a 0): ")
+
+
+x = raw_input("Ingrese simbolo de largo 1 que represente a todos sus estados (Ej: q => q0, q1, ..., qn): ")
+while(len(x)!=1):
+      x = raw_input("Ingrese simbolo de largo 1 que represente a todos sus estados (Ej: q => q0, q1, ..., qn): ")
+
+for i in range(n):
+        listaestados.append(Estado(x+str(i)))
+
+#demostracion
+for i in range(n):
+        print listaestados[i].nombre
+
+
+#interfaz grafica
