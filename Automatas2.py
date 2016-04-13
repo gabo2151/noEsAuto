a814d3c19521961a52589517b82a093be32c2bf0
eidepozo <eidepozo@hotmail.com>
2016-04-13 19:40:15 -0300

    un poco mas lindi

--- Automatas2.py
+++ Automatas2.py
@@ -1,5 +1,6 @@
 from Tkinter import *
 
+#Clase Estado y sus atributos
 class Estado:
 	def __init__(self, nombre):
 		self.nombre = nombre
@@ -7,28 +8,18 @@ class Estado:
 		self.final = False
 		self.indices = {}
 
-	# Sirve para editar tambien***
-	def addIndice(self, caracter, direccion):
-		# La direccion debiese de ser el numero del indice
-		self.indices[caracter] = direccion
-
-	def data(self):
-		return self.indices
-
-
-#funciones basicas
+#Funciones basicas
 def muestraAtributos():
 	for i in range(len(listaestados)):
 		print listaestados[i].nombre
 		print listaestados[i].indices
 
-def validaEstado(nombreestado):
+def validaEstado(nombreestado): #devuelve verdadero si el estado se encuentra en listaestados, falso si no
 	for i in range(len(listaestados)):
 		if listaestados[i].nombre == nombreestado:
 			return True
 			break
 	return False
-
 
 def validaTrans(pos,clave): #devuelve verdadero si la clave se encuentra en estado que se encuentra en pos
 	if clave in listaestados[pos].indices:
@@ -36,34 +27,29 @@ def validaTrans(pos,clave): #devuelve verdadero si la clave se encuentra en esta
 	else:
 		return False
 
-def devuelvePos(nombreestado):#busca el estado en la lista y devuelve su posicion
+def devuelvePos(nombreestado): #busca el estado en la lista y devuelve su posicion
 	for i in range(len(listaestados)):
 		if listaestados[i].nombre == nombreestado:
 			return i
 			break
 
-
-#Nuevas funciones (cuando se incluyo gui estas surgieron por necesidad)
-
-def estadoInicial():
-	if entry1.get() != "": #si la entrada es distinta de vacio, ajusto los colores a los por defecto y creo el estado inicial
-		entry1.config(highlightbackground="#d9d9d9")
-		entry1.config(highlightcolor="black")
+#Funciones que interactuan directamente con la gui
+def estadoInicial():# crea y define al estado inicial si la entrada es distinta a vacio
+	if entry1.get() != "": 
+		entry1.config(highlightbackground="#d9d9d9")# donde se vea este tipo de "comando" en el codigo, se esta realizando un cambio de color
+		entry1.config(highlightcolor="black")# en los marcos de las entradas
 		listaestados.append(Estado(entry1.get()))
 		listaestados[0].inicial=True
 		button1.configure(state=DISABLED)
 		button1.update()
 		text1.insert(INSERT,"- El estado inicial es (%s).\n" %(entry1.get()))
-
-	else: #si no, el borde se vuelve rojo cuando esta enfocado y cuando no en la entrada
+	else:
 		entry1.config(highlightbackground="red")
 		entry1.config(highlightcolor="red")
 
-
-
-def creaTransiciones():
-	l = entry2.get().split()
-	verifica = True
+def creaTransiciones():# crea las transiciones de sintaxis: estado simbolo estado, considerando si estos ya existen, y si existen transiciones asociadas
+	l = entry2.get().split()# a dicho simbolo, para el primer estado en el ingreso, guardando (de no existir) el simbolo y la clave (segundo estado) en
+	verifica = True #su propio diccionario indices
 	if len(listaestados)!= 0 and len(l) == 3: #solo si existen estados dentro de listaestados y si el largo de mi lista es 3 creare transiciones
 		entry2.config(highlightbackground="#d9d9d9") #con widget.cget('highlightbackground') consegui este valor ty internet
 		entry2.config(highlightcolor="black")
@@ -97,7 +83,7 @@ def creaTransiciones():
 		entry2.config(highlightbackground="red") #siempre que sea != de 3 el largo de la lista, el borde contenedor sera rojo
 		entry2.config(highlightcolor="red")
 
-def estadosFinales():
+def estadosFinales():# redefine el atributo final de los estados en la listaestados segun el ingreso del usuario, si no existen los estados no hace nada
 	if entry3.get() != "":
 		entry3.config(highlightbackground="#d9d9d9")
 		entry3.config(highlightcolor="black")
@@ -114,41 +100,8 @@ def estadosFinales():
 		entry3.config(highlightbackground="red")
 		entry3.config(highlightcolor="red")
 
-
-
-def verificaPalabra2(): #el del gabo
-	eA = 0 #Estado actual
-	cuenta = 0
-	testWord = entry4.get()
-	if testWord != "":
-		entry4.config(highlightbackground="#d9d9d9")
-		entry4.config(highlightcolor="black")
-		for key in testWord:
-			if key in listaestados[eA].indices: #si el caracter se encuentra como clave en el diccionario indices
-				current = listaestados[eA].indices[key] #defino current como el valor asociado al caracter (un estado)
-				for i in range(len(listaestados)):
-					if current == listaestados[i].nombre:
-						eA = i
-						break
-				cuenta +=1
-			else:
-				text1.insert(INSERT,"- La palabra (%s) no pertenece al lenguaje.\n" %(testWord))
-				break
-			if cuenta == len(testWord):
-				if listaestados[eA].final:
-					text1.insert(INSERT,"- La palabra (%s) pertenece al lenguaje.\n" %(testWord))
-					break
-				else:
-					text1.insert(INSERT,"- La palabra (%s) no pertenece al lenguaje.\n" %(testWord))
-					break
-	else:
-		entry4.config(highlightbackground="red")
-		entry4.config(highlightcolor="red")
-
-
-
-def verificaPalabra(): #nuevo ave fenix
-	eA = 0 # estado actual
+def verificaPalabra(): #va evaluando caracter a caracter la palabra, moviendose entre los estados, sus claves y los valores asociados a las claves
+	eA = 0 #determinando finalmente, si se encuentra en un estado final, que la palabra pertenece al lenguaje
 	cuenta = 0
 	testWord = entry4.get()
 	if testWord != "":
@@ -174,10 +127,7 @@ def verificaPalabra(): #nuevo ave fenix
 		entry4.config(highlightbackground="red")
 		entry4.config(highlightcolor="red")
 
-
-#Funciones interfaz grafica
-
-def activar(): #funcion que enlaza el cbutton1 con el button2
+def activar(): # enlaza el cbutton1 con el button2
     if CheckVar1.get() == 1:
     	button2.configure(state=NORMAL)
     	button2.update()
@@ -185,12 +135,13 @@ def activar(): #funcion que enlaza el cbutton1 con el button2
     	button2.configure(state=DISABLED)
     	button2.update()
 
-def limpiar():
+def limpiar(): # limpia la consola de procesos
 	text1.delete('1.0', END) #borro desde la primera fila hasta elfinal
 	#text1.insert(INSERT,"\n")
 
-def hardReset():#esta funcion se encarga de volver todo a su valor original
+def hardReset():# se encarga de volver todo a su valor original para asi no reiniciar el programa a cada rato
 	listaestados[:] = [] #elimino de mi lista estados todos los estados
+	listaentradas = [entry1, entry2, entry3, entry4] #cree esta lista para manipular mas facilmente los objetos tipo entry
 	text1.delete('1.0', END) #borro los datos de la consola
 	button1.configure(state=NORMAL)#nueva consideracion
 	cbutton1.deselect() #en caso de que el cbutton este seleccionado, lo deselecciono
@@ -201,82 +152,71 @@ def hardReset():#esta funcion se encarga de volver todo a su valor original
 			listaentradas[i].config(highlightbackground="#d9d9d9")
 			listaentradas[i].config(highlightcolor="black")
 
-
-#Variables
-listaestados = [] #lista de objetos tipo estado
-listaentradas = [] #lista de objetos tipo entry
-
-#Interfaz Grafica  
-root= Tk()
-root.title("Simulador de AFD")
-root.geometry("510x580") #anchoxalto
-
-
-frame1 = Frame(root,width=510,height=575)
-frame1.pack()
-
-CheckVar1 = IntVar()
-
-label1 = Label(frame1, text="Estado inicial")
-label1.place(x=25, y=20)
-
-entry1 = Entry(frame1,width=7)
-entry1.place(x=125, y=20)
-listaentradas.append(entry1)
-
-button1= Button(frame1,text="Definir", command=estadoInicial)
-button1.place(x=195, y=15)
+if __name__ == "__main__":
+	root= Tk()
+	root.title("Simulador de AFD")
+	root.geometry("510x580") #anchoxalto
 	
-label2 = Label(frame1, text="Transiciones")
-label2.place(x=25, y=55)
+	frame1 = Frame(root, width=510, height=575)
+	frame1.pack()
 
-label3 = Label(frame1, text="Ej: q0 a q1")
-label3.place(x=125, y=77)
+	listaestados = [] #lista de objetos tipo estado
+	CheckVar1 = IntVar()#variable inteligente
 
-entry2 = Entry(frame1,width=20)
-entry2.place(x=125, y=55)
-listaentradas.append(entry2)
+	label1 = Label(frame1, text="Estado inicial")
+	label1.place(x=25, y=20)
 
-cbutton1= Checkbutton(frame1,text="Confirmar", variable=CheckVar1, onvalue=1, offvalue=0, command=activar)
-cbutton1.place(x=300, y=55)
+	entry1 = Entry(frame1, width=7)
+	entry1.place(x=125, y=20)
 
-button2 = Button(frame1, state=DISABLED, text="Agregar", command=creaTransiciones)
-button2.place(x=400, y=50)
+	button1= Button(frame1, text="Definir", command=estadoInicial)
+	button1.place(x=195, y=15)
+		
+	label2 = Label(frame1, text="Transiciones")
+	label2.place(x=25, y=55)
 
-label4 = Label(frame1, text="Estados finales")
-label4.place(x=25, y=110)
+	label3 = Label(frame1, text="Ej: q0 a q1")
+	label3.place(x=125, y=77)
 
-entry3 = Entry(frame1, width=25)
-entry3.place(x=125, y=110)
-listaentradas.append(entry3)
+	entry2 = Entry(frame1,width=20)
+	entry2.place(x=125, y=55)
 
-label5 = Label(frame1, text="Ej: q0 q1 q2 ...")
-label5.place(x=125, y=132)
+	cbutton1= Checkbutton(frame1, text="Confirmar", variable=CheckVar1, onvalue=1, offvalue=0, command=activar)
+	cbutton1.place(x=300, y=55)
 
-button3 = Button(frame1, text="Definir",command=estadosFinales)
-button3.place(x=340, y=105)
+	button2 = Button(frame1, state=DISABLED, text="Agregar", command=creaTransiciones)
+	button2.place(x=400, y=50)
 
-label6 = Label(frame1, text="Palabra")
-label6.place(x=25, y=165)
+	label4 = Label(frame1, text="Estados finales")
+	label4.place(x=25, y=110)
 
-entry4 = Entry(frame1, width=25)
-entry4.place(x=125, y=165)
-listaentradas.append(entry4)
+	entry3 = Entry(frame1, width=25)
+	entry3.place(x=125, y=110)
 
-button4 = Button(frame1, text="Evaluar", command=verificaPalabra)
-button4.place(x=340, y=160)
+	label5 = Label(frame1, text="Ej: q0 q1 q2 ...")
+	label5.place(x=125, y=132)
 
-label6 = Label(frame1, text="Consola de Procesos")
-label6.place(x=25, y=210)
+	button3 = Button(frame1, text="Definir",command=estadosFinales)
+	button3.place(x=340, y=105)
 
-text1 = Text(frame1,width=65, height=20, wrap=WORD)
-text1.place(x=25, y=230)
+	label6 = Label(frame1, text="Palabra")
+	label6.place(x=25, y=165)
 
-button5 = Button(frame1, text="Limpiar Consola",command=limpiar)
-button5.place(x=280, y=545)
+	entry4 = Entry(frame1, width=25)
+	entry4.place(x=125, y=165)
 
-button6 = Button(frame1, text="Reiniciar",command=hardReset)
-button6.place(x=410, y=545)
+	button4 = Button(frame1, text="Evaluar", command=verificaPalabra)
+	button4.place(x=340, y=160)
 
-root.mainloop()
+	label6 = Label(frame1, text="Consola de Procesos")
+	label6.place(x=25, y=210)
 
+	text1 = Text(frame1, width=65, height=20, wrap=WORD)
+	text1.place(x=25, y=230)
+
+	button5 = Button(frame1, text="Limpiar Consola", command=limpiar)
+	button5.place(x=280, y=545)
+
+	button6 = Button(frame1, text="Reiniciar", command=hardReset)
+	button6.place(x=410, y=545)
+	root.mainloop()
